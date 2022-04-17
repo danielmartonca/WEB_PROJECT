002f0b23f9b7c1cd3522344ee585cf27ed6020a8
ronadumi <79197571+ronadumi@users.noreply.github.com>
2022-04-17 20:05:19 +0300

    in progress

---
+++ JS/dropdown.js
@@ -0,0 +1,29 @@
+var dropdown = document.getElementsByClassName("dropdown");
+var i;
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+
+function myFunction() {
+  var y = document.getElementById("responsive-navigation");
+  if (y.className === "navigation") {
+    y.className += " responsive";
+  } else {
+    y.className = "navigation";
+  }
+  var x = document.getElementById("sideNav");
+  if (x.className === "main") {
+    x.className += " responsive";
+  } else {
+    x.className = "main";
+  }
+}
