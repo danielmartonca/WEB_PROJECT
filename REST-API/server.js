08294e6b386ff45297024ad9ba42e953bd738947
Martoncă Daniel <45790572+danielmartonca@users.noreply.github.com>
2022-06-19 16:56:51 +0300

    Added jwt to the server

--- REST-API/server.js
+++ REST-API/server.js
@@ -3,19 +3,26 @@ const httpUtils = require('./utilities/http-utils')
 const {StatusCode} = require('status-code-enum')
 const getController = require('./controllers/get-controller').getController;
 const postController = require('./controllers/post-controller').postController;
+const jwtService = require('./services/jwt-service');
 
 const PORT = 8083;
 
+const authorisationRequiredApis = new Set(["/MainPage.html", "/About.html", //TODO add others
+]);
+
 http.createServer(function (request, response) {
+    //while data is still being received -> do nothing
     let data = '';
     request.on('data', (chunk) => {
         data += chunk;
     });
 
-    request.on('end', () => {
+    //after all the data has been retrieved
+    request.on('end', async () => {
         let json = '';
         if (data !== '') {
             try {
+                //transform it into json
                 json = JSON.parse(data);
             } catch (e) {
                 console.error(`Couldn't convert request body to json.Request body is:${'\n' + JSON.stringify(data)}`)
@@ -24,7 +31,33 @@ http.createServer(function (request, response) {
             }
         }
 
+        //log all requests that are not GET
         if (request.method !== "GET") console.log(`REQUEST:       ${request.method} ${request.url}${'\n' + JSON.stringify(json)}\n`);
+
+        //if the current api call requires authorisation look for 'Authorisation: Bearer tokenValue' header and check if tokenValue is a valid JWT token then verify it
+        if (authorisationRequiredApis.has(request.url)) {
+            if (request.headers.authorization === undefined) {
+                console.log("User tried to access confidential resource without authorisation header.")
+                httpUtils.buildResponse(response, StatusCode.ClientErrorUnauthorized, {'ContentType': 'text/plain'}, `Not authorised to access the resource.`)
+                return;
+            }
+
+            if (!request.headers.authorization.includes('Bearer ')) {
+                console.log("User tried to access confidential resource without authorisation header:  'Bearer tokenValue'.")
+                httpUtils.buildResponse(response, StatusCode.ClientErrorUnauthorized, {'ContentType': 'text/plain'}, `Not authorised to access the resource.`)
+                return;
+            }
+
+            let token = request.headers.authorization.split(' ')[1];
+            let isValid = await jwtService.verifyJwt(token);
+            if (!isValid) {
+                console.log("User tried to access unauthorised resource with invalid JWT token.")
+                httpUtils.buildResponse(response, StatusCode.ClientErrorUnauthorized, {'ContentType': 'text/plain'}, `Not authorised to access the resource.`)
+                return;
+            }
+
+            console.log("Token found and validated.");
+        }
 
         switch (request.method) {
             case "GET":
