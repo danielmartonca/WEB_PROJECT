8f4afd192a03a0ab0c206c8aded7e288592cea00
Martoncă Daniel <45790572+danielmartonca@users.noreply.github.com>
2022-06-19 21:23:08 +0300

    Added api to get pet details and added to registration automatic creation of pet details in database

--- REST-API/controllers/get-controller.js
+++ REST-API/controllers/get-controller.js
@@ -7,12 +7,7 @@ const {StatusCode} = require('status-code-enum')
 async function getController(request, response, jwt = "") {
     switch (request.url) {
         case "/getPetDetails":
-            const account = await jwtService.extractAccount(jwt);
-            if (account == null) {
-                httpUtils.buildResponse(response, StatusCode.ServerErrorInternal, null, "An unexpected error has occurred.");
-                return;
-            }
-            await animalsService.getPetDetails(request, response, account);
+            await animalsService.getPetDetails(request, response, jwt);
             break;
         default:
             baseService.handleFiles(request, response);
--- REST-API/services/animals-service.js
+++ REST-API/services/animals-service.js
@@ -1,9 +1,17 @@
 const databaseService = require('../services/database-service');
 const httpUtils = require('../utilities/http-utils')
 const {StatusCode} = require('status-code-enum')
+const jwtService = require("./jwt-service");
 
 
-async function getPetDetails(request, response, account) {
+async function getPetDetails(request, response, jwt) {
+    //extract account from jwt
+    const account = await jwtService.extractAccount(jwt);
+    if (account == null) {
+        httpUtils.buildResponse(response, StatusCode.ServerErrorInternal, null, "An unexpected error has occurred.");
+        return;
+    }
+
     //extract pet details from database
     let petDetails = await databaseService.getPetDetails(account.email, account.password);
     if (petDetails === null) {
